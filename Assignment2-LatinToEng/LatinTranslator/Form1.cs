4fcae296ebb3348409a3a025a2a0c6fc3965c663
Senahilal <113223453+Senahilal@users.noreply.github.com>
2024-02-03 12:44:07 -0500

    Add files via upload

---
+++ Assignment2-LatinToEng/LatinTranslator/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatinTranslator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void txtEng3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void txtEng2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLatin3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLatin2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLatin1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Up"; 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtEng2.Text = "Down";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtEng3.Text = "center";
+        }
+    }
+}
