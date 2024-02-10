c2e95234acdaa71f3a588bb23996bfde556fe9ff
tdamani <123122240+tdamani@users.noreply.github.com>
2024-02-10 16:09:39 -0500

    Add files via upload

---
+++ sealevelListBox/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnC = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(290, 194);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(166, 61);
+            this.btnC.TabIndex = 0;
+            this.btnC.Text = "Calculate";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(270, 120);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(213, 31);
+            this.txt1.TabIndex = 1;
+            this.txt1.Text = "Enter number";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(335, 315);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(73, 25);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ sealevelListBox/sealevelListBox/FactorialCalc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sealevelListBox
+{
+    public partial class FactorialCalc : Form
+    {
+        public FactorialCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void PopulateListBox()
+        {
+            const double annualRise = 1.5; // Ocean's level rising rate per year in millimeters
+            for (int year = 1; year <= 10; year++)
+            {
+                double totalRise = annualRise * year;
+                listBox1.Items.Add($"Year {year}: {totalRise} mm");
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            PopulateListBox();
+        }
+    }
+}
