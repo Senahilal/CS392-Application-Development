866f825c125c41af140fe2ce49ef50452abec08c
Sena Bayram <senab@bu.edu>
2024-03-05 12:34:01 -0500

    Assignment 6

---
+++ Lecture6-Assignment6/Assignment6/hangman/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hangman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void A_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public int isInWord(String s, char c)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
