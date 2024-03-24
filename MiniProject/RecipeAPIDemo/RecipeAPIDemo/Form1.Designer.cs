76d133668dd83aa0592a1f174e8b6bb9d784ef76
Mya Lucas <myalucas@bu.edu>
2024-03-24 16:46:18 -0400

    Add recipe api

---
+++ MiniProject/RecipeAPIDemo/RecipeAPIDemo/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SerpAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(25, 35);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(205, 20);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(335, 32);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(84, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.Location = new System.Drawing.Point(25, 65);
+            this.resultsListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(515, 134);
+            this.resultsListBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter ingredients, separated by comma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 227);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListBox resultsListBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MiniProject/RecipeAPIDemo/RecipeAPIDemo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace SerpAPITest
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            searchButton.Enabled = false; // Prevents multiple clicks
+
+            string apiKey = "key-goes-here";
+            string query = searchTextBox.Text;
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"https://api.spoonacular.com/recipes/findByIngredients?&ingredients={Uri.EscapeDataString(query)}&number=3&sort=popularity"))
+            {
+                try
+                {
+                    requestMessage.Headers.Add("x-api-key", apiKey);
+                    // Asynchronously call the API and await the response
+                    HttpResponseMessage resp = await client.SendAsync(requestMessage);
+
+                    // Parse the JSON response
+                    JArray data = JArray.Parse(await resp.Content.ReadAsStringAsync());
+
+                    // Update UI (ensure execution on UI thread)
+                    UpdateResultsList(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    searchButton.Enabled = true; // Re-enable the button
+                }
+            }
+
+        }
+        private void UpdateResultsList(JToken recipeResults)
+        {
+            resultsListBox.Items.Clear();
+
+            int resultCount = 0;
+            foreach (var result in recipeResults)
+            {
+                string title = result["title"].ToString();
+                string missedIngredientCount = result["missedIngredientCount"].ToString();
+                string likes = result["likes"].ToString();
+
+                resultsListBox.Items.Add($"Title: {title}, Missing Ingredients: {missedIngredientCount}, Likes: {likes}");
+
+                resultCount++;
+            }
+        }
+    }
+}
