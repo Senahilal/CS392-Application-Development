b27b7404eedd3e31aacca29ca11daeb657767cea
tdamani <123122240+tdamani@users.noreply.github.com>
2024-03-24 15:25:54 -0400

    added SERPAPI (google shopping) test app

---
+++ MiniProject/SerpAPITest/SerpAPITest/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SerpAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(137, 60);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(213, 31);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(463, 60);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(169, 45);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.ItemHeight = 25;
+            this.resultsListBox.Location = new System.Drawing.Point(50, 125);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(698, 304);
+            this.resultsListBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListBox resultsListBox;
+    }
+}
+
---
+++ MiniProject/SerpAPITest/SerpAPITest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace SerpAPITest
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            searchButton.Enabled = false; // Prevents multiple clicks
+
+            string apiKey = "c59cba7a52e840ccf946dd9fe9ab07ef917055952b19c51cfa4e70dfc26c67b8";
+            string query = searchTextBox.Text;
+            string url = $"https://serpapi.com/search.json?engine=google_shopping&q={Uri.EscapeDataString(query)}&api_key={apiKey}";
+
+            try
+            {
+                // Asynchronously call the API and await the response
+                string responseString = await client.GetStringAsync(url);
+
+                // Parse the JSON response
+                JObject data = JObject.Parse(responseString);
+
+                // Assuming 'shopping_results' is part of your JSON response
+                var shopping_results = data["shopping_results"];
+
+                // Update UI (ensure execution on UI thread)
+                UpdateResultsList(shopping_results);
+            }
+            catch (Exception ex) // Consider catching more specific exceptions
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                searchButton.Enabled = true; // Re-enable the button
+            }
+        }
+        private void UpdateResultsList(JToken shoppingResults)
+        {
+            resultsListBox.Items.Clear();
+
+            int resultCount = 0;
+            foreach (var result in shoppingResults)
+            {
+                if (resultCount >= 3) break; // Display only top 3 results
+
+                string title = result["title"].ToString();
+                string price = result["price"].ToString();
+                string source = result["source"].ToString();
+
+                resultsListBox.Items.Add($"Title: {title}, Price: {price}, Source: {source}");
+
+                resultCount++;
+            }
+        }
+    }
+}
