7580f55b7116b2384aea1c471e0af9f419b20948
Mya Lucas <myalucas@bu.edu>
2024-03-25 23:00:28 -0400

    Add chat gpt demo

---
+++ MiniProject/ChatGPTDemo/ChatGPTDemo/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SerpAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(103, 11);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(640, 58);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(766, 32);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(84, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a prompt";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(25, 74);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(845, 443);
+            this.resultBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 542);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox resultBox;
+    }
+}
+
---
+++ MiniProject/ChatGPTDemo/ChatGPTDemo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace SerpAPITest
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            searchButton.Enabled = false; // Prevents multiple clicks
+
+            string apiKey = "key-goes-here";
+            string query = searchTextBox.Text;
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, $"https://api.openai.com/v1/chat/completions"))
+            {
+                try
+                {
+                    var jsonBody = new
+                    {
+                        model = "gpt-3.5-turbo",
+                        messages = new[]
+                        {
+                            new { role = "system", content = "You are a helpful assistant who provides suggestions on meals to prepare based on provided criteria." },
+                            new { role = "user", content = searchTextBox.Text }
+                        }
+                    };
+
+                    // Serialize the JSON body
+                    string jsonBodyString = JsonConvert.SerializeObject(jsonBody);
+
+                    requestMessage.Content = new StringContent(jsonBodyString);
+
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                    requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    // Asynchronously call the API and await the response
+                    HttpResponseMessage resp = await client.SendAsync(requestMessage);
+
+                    // Parse the JSON response
+                    JObject data = JObject.Parse(await resp.Content.ReadAsStringAsync());
+
+                    // Update UI (ensure execution on UI thread)
+                    UpdateResultsList(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    searchButton.Enabled = true; // Re-enable the button
+                }
+            }
+
+        }
+        private void UpdateResultsList(JToken results)
+        {
+            string normalized = Regex.Replace(results["choices"][0]["message"]["content"].ToString(), @"\r\n|\n\r|\n|\r", "\r\n");
+            resultBox.Text = normalized;
+        }
+    }
+}
